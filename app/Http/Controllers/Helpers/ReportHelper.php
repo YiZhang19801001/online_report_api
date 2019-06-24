69e233d221b1d96eb4492cf79a5185b950ec2fe0
china9911 <yizhang19801001@outlook.com>
2019-06-25 09:42:15 +1000

    ReportController@store

--- app/Http/Controllers/Helpers/ReportHelper.php
+++ app/Http/Controllers/Helpers/ReportHelper.php
@@ -131,7 +131,7 @@ class ReportHelper
         // set connection database ip in run time
         \Config::set('database.connections.sqlsrv.host', $shop->database_ip);
         // read all dockets during the period
-        $sql = Docket::with("docketlines")->whereBetween('docket_date', [$this->yesterday, $this->today])->where('transaction', "SA")->orWhere('transaction', "IV");
+        $sql = Docket::with("docketlines")->whereBetween('docket_date', [$startDate, $endDate])->where('transaction', "SA")->orWhere('transaction', "IV");
 
         return ['totalSales' => $sql->sum('total_inc'), 'totalTx' => $sql->count(), 'shop' => $shop];
     }
