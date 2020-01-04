1abbb6754509787d7e4264423ee896cf721d2d89
roben <yizhang19801001@gmail.com>
2020-01-04 15:46:18 +1100

    return errors for 500

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -151,96 +151,100 @@ class ReportController extends Controller
 
     public function store(Request $request)
     {
-        #read inputs
-        $today = new \DateTime('now', new \DateTimeZone('Australia/Sydney'));
+        try {
+            #read inputs
+            $today = new \DateTime('now', new \DateTimeZone('Australia/Sydney'));
 
-        $startDate = new \DateTime($request->startDate, new \DateTimeZone('Australia/Sydney'));
-        $endDate = new \DateTime($request->endDate, new \DateTimeZone('Australia/Sydney'));
+            $startDate = new \DateTime($request->startDate, new \DateTimeZone('Australia/Sydney'));
+            $endDate = new \DateTime($request->endDate, new \DateTimeZone('Australia/Sydney'));
 
-        $user = $request->user();
+            $user = $request->user();
 
-        if ($user->user_type === 'CUSTOMER') {
-            // find shop according to inputs shop_ip
-            $shops = $user->shops()->get();
+            if ($user->user_type === 'CUSTOMER') {
+                // find shop according to inputs shop_ip
+                $shops = $user->shops()->get();
 
-            #call helper class to generate data
-            $result = $this->helper->getTotalSummary($shops, $startDate, $endDate, $user);
-            // use switch to filter the meta in controller make codes more readable in helper class
-            $reports = $result['reports'];
-            $shops = $result['shops'];
-        } else if ($user->user_type === 'HEAD') {
-            // find shop according to inputs shop_ip
-            $shopId = $user->shops()->first()->shop_id;
+                #call helper class to generate data
+                $result = $this->helper->getTotalSummary($shops, $startDate, $endDate, $user);
+                // use switch to filter the meta in controller make codes more readable in helper class
+                $reports = $result['reports'];
+                $shops = $result['shops'];
+            } else if ($user->user_type === 'HEAD') {
+                // find shop according to inputs shop_ip
+                $shopId = $user->shops()->first()->shop_id;
 
-            $shop = Shop::find($shopId);
+                $shop = Shop::find($shopId);
 
-            DB::purge();
+                DB::purge();
 
-            // set connection database ip in run time
-            \Config::set('database.connections.sqlsrv.host', $shop->database_ip);
-            \Config::set('database.connections.sqlsrv.username', $shop->username);
-            \Config::set('database.connections.sqlsrv.password', $shop->password);
-            \Config::set('database.connections.sqlsrv.database', $shop->database_name);
-            \Config::set('database.connections.sqlsrv.port', $shop->port);
+                // set connection database ip in run time
+                \Config::set('database.connections.sqlsrv.host', $shop->database_ip);
+                \Config::set('database.connections.sqlsrv.username', $shop->username);
+                \Config::set('database.connections.sqlsrv.password', $shop->password);
+                \Config::set('database.connections.sqlsrv.database', $shop->database_name);
+                \Config::set('database.connections.sqlsrv.port', $shop->port);
 
-            $shops = PosHeadShop::where('shop_id', '>', 0)->where('inactive', '!=', 1)->get();
-            #call helper class to generate data
-            $reports = $this->headHelper->getTotalSummary($shops, $startDate, $endDate, $user);
+                $shops = PosHeadShop::where('shop_id', '>', 0)->where('inactive', '!=', 1)->get();
+                #call helper class to generate data
+                $reports = $this->headHelper->getTotalSummary($shops, $startDate, $endDate, $user);
 
-        } else if ($user->user_type === "GIFTSHOPHEAD") {
-            // find shop according to inputs shop_ip
-            $shop = $user->shops()->first();
+            } else if ($user->user_type === "GIFTSHOPHEAD") {
+                // find shop according to inputs shop_ip
+                $shop = $user->shops()->first();
 
-            DB::purge();
+                DB::purge();
 
-            // set connection database ip in run time
-            \Config::set('database.connections.sqlsrv.host', $shop->database_ip);
-            \Config::set('database.connections.sqlsrv.username', $shop->username);
-            \Config::set('database.connections.sqlsrv.password', $shop->password);
-            \Config::set('database.connections.sqlsrv.database', $shop->database_name);
-            \Config::set('database.connections.sqlsrv.port', $shop->port);
+                // set connection database ip in run time
+                \Config::set('database.connections.sqlsrv.host', $shop->database_ip);
+                \Config::set('database.connections.sqlsrv.username', $shop->username);
+                \Config::set('database.connections.sqlsrv.password', $shop->password);
+                \Config::set('database.connections.sqlsrv.database', $shop->database_name);
+                \Config::set('database.connections.sqlsrv.port', $shop->port);
 
-            $shops = PosHeadShop::where('shop_id', '>', 0)->where('inactive', 0)->get();
-            #call helper class to generate data
-            switch ($request->reportType) {
-                case 'shop':
-                    $reports = $this->giftShopHeadHelper->getShopTotalSummary($shops, $startDate, $endDate, $user);
-                    break;
-                case 'group':
-                    $groupId = $request->groupId;
-                    $reports = $this->giftShopHeadHelper->getGroupSalesSummary($shops, $startDate, $endDate, $groupId, $user);
-                    break;
-                case 'agent':
-                    if ($user->name === 'lisa') {
-                        $groupNames = TourGroup::
-                            whereBetween('start_date', [$startDate, $endDate])
-                            ->select('group_code')->get();
-                        $groupNameToPax = TourGroup::
-                            whereBetween('start_date', [$startDate, $endDate])
-                            ->select('group_code as group_name', 'pax')->get();
-                    } else {
-                        $groupNames = TourGroup::
-                            whereBetween('date_start', [$startDate, $endDate])
-                            ->select('group_name')->get();
+                $shops = PosHeadShop::where('shop_id', '>', 0)->where('inactive', 0)->get();
+                #call helper class to generate data
+                switch ($request->reportType) {
+                    case 'shop':
+                        $reports = $this->giftShopHeadHelper->getShopTotalSummary($shops, $startDate, $endDate, $user);
+                        break;
+                    case 'group':
+                        $groupId = $request->groupId;
+                        $reports = $this->giftShopHeadHelper->getGroupSalesSummary($shops, $startDate, $endDate, $groupId, $user);
+                        break;
+                    case 'agent':
+                        if ($user->name === 'lisa') {
+                            $groupNames = TourGroup::
+                                whereBetween('start_date', [$startDate, $endDate])
+                                ->select('group_code')->get();
+                            $groupNameToPax = TourGroup::
+                                whereBetween('start_date', [$startDate, $endDate])
+                                ->select('group_code as group_name', 'pax')->get();
+                        } else {
+                            $groupNames = TourGroup::
+                                whereBetween('date_start', [$startDate, $endDate])
+                                ->select('group_name')->get();
 
-                        $groupNameToPax = TourGroup::
-                            whereBetween('date_start', [$startDate, $endDate])
-                            ->select('group_name', 'pax')->get();
+                            $groupNameToPax = TourGroup::
+                                whereBetween('date_start', [$startDate, $endDate])
+                                ->select('group_name', 'pax')->get();
 
-                    }
-                    // var_dump($groupNameToPax);
-                    // return response()->json(compact('groupNameToPax'), 200);
-                    $agentName = $request->input("agentName", "");
-                    $reports = $this->giftShopHeadHelper->getAgentSalesSummary($shops, $startDate, $endDate, $user, $agentName, $groupNames, $groupNameToPax);
+                        }
+                        // var_dump($groupNameToPax);
+                        // return response()->json(compact('groupNameToPax'), 200);
+                        $agentName = $request->input("agentName", "");
+                        $reports = $this->giftShopHeadHelper->getAgentSalesSummary($shops, $startDate, $endDate, $user, $agentName, $groupNames, $groupNameToPax);
 
-                    break;
-                default:
-                    $reports = $this->giftShopHeadHelper->getShopTotalSummary($shops, $startDate, $endDate, $user);
-                    break;
+                        break;
+                    default:
+                        $reports = $this->giftShopHeadHelper->getShopTotalSummary($shops, $startDate, $endDate, $user);
+                        break;
+                }
             }
+            $path = 'totalSummary';
+            return response()->json(compact('reports', 'path', 'shops'), 200);
+        } catch (\Throwable $th) {
+            return response()->json(['errors' => $th->getMessage()], 400);
         }
-        $path = 'totalSummary';
-        return response()->json(compact('reports', 'path', 'shops'), 200);
     }
 
     public function update(Request $request, $id)
