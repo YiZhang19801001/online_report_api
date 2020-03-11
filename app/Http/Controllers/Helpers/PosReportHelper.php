3a79f3dff27b21032d0c4c533cdb46a3086c34d6
roben <yizhang19801001@gmail.com>
2020-03-11 18:05:05 +1100

    stock_id < 0 add to reports

--- app/Http/Controllers/Helpers/PosReportHelper.php
+++ app/Http/Controllers/Helpers/PosReportHelper.php
@@ -282,8 +282,6 @@ class PosReportHelper
                 # read all dockets during the period
                 $sqlResult = DB::connection('sqlsrv')->table('DocketLine')
                     ->join('Docket', 'DocketLine.docket_id', '=', 'Docket.docket_id')
-                    ->join('Stock', 'Stock.stock_id', '=', 'DocketLine.stock_id')
-                    ->where('Stock.stock_id', '>', 0)
                     ->whereBetween('Docket.docket_date', [$startDate, $endDate])
                     ->whereIn('Docket.transaction', ["SA", "IV"])
                     ->selectRaw('sum((DocketLine.sell_ex - DocketLine.cost_ex) * DocketLine.quantity) as gp ,sum((DocketLine.RRP - DocketLine.sell_inc)*DocketLine.quantity) as discount,count(DISTINCT Docket.Docket_id) as totalTx,sum(DocketLine.sell_inc * DocketLine.quantity) as totalSales,sum(abs(DocketLine.sell_inc * DocketLine.quantity)) as absTotal,sum(DocketLine.sell_ex * DocketLine.quantity) as totalSales_ex')
@@ -292,8 +290,6 @@ class PosReportHelper
                 # calculate totalRefund
                 $sqlResult2 = DB::connection('sqlsrv')->table('DocketLine')
                     ->join('Docket', 'DocketLine.docket_id', '=', 'Docket.docket_id')
-                    ->join('Stock', 'Stock.stock_id', '=', 'DocketLine.stock_id')
-                    ->where('Stock.stock_id', '>', 0)
                     ->whereBetween('Docket.docket_date', [$startDate, $endDate])
                     ->whereIn('Docket.transaction', ["SA", "IV"])
                     ->where('DocketLine.quantity', '<', 0)
